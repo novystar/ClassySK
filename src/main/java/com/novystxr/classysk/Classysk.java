e747a2d49b54f2d69dccb91b1ad8f6f73882a7f5
nova ⭐ <novaa0@outlook.com>
2026-06-02 00:54:26 +0100

    change implementation of parse time context validation

--- src/main/java/com/novystxr/classysk/Classysk.java
+++ src/main/java/com/novystxr/classysk/Classysk.java
@@ -4,7 +4,6 @@ import ch.njol.skript.ScriptLoader;
 import ch.njol.skript.Skript;
 import ch.njol.skript.lang.parser.ParserInstance;
 import com.novystxr.classysk.api.classes.CleanUpListener;
-import com.novystxr.classysk.api.classes.ParserClassData;
 import com.novystxr.classysk.main.MainModule;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.skriptlang.skript.addon.SkriptAddon;
@@ -14,7 +13,6 @@ public final class Classysk extends JavaPlugin {
     @Override
     public void onEnable() {
         SkriptAddon addon = Skript.instance().registerAddon(Classysk.class, "ClassySK");
-        ParserInstance.registerData(ParserClassData.class, ParserClassData::new);
         ScriptLoader.eventRegistry().register(new CleanUpListener());
         addon.loadModules(new MainModule());
     }
