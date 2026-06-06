5b2d717791c89802259a78c26f82911993f17025
nova ⭐ <novaa0@outlook.com>
2026-06-06 12:23:25 +0100

    fix FieldSignature#canConvert plurality check

--- src/main/java/com/novystxr/classysk/api/fields/SkriptField.java
+++ src/main/java/com/novystxr/classysk/api/fields/SkriptField.java
@@ -40,7 +40,7 @@ public class SkriptField {
 
         public boolean canConvert(Object[] values) {
             if (values == null) return true;
-            if (values.length != 1 && isPlural) return false;
+            if (values.length != 1 && !isPlural) return false;
 
             return ConverterUtils.canConvert(type, values);
         }
