b87d2b3cb3ed4a2d96adc2afec7cadaf56ddf707
nova ⭐ <novaa0@outlook.com>
2026-06-08 14:04:27 +0100

    clean up code

--- src/main/java/com/novystxr/classysk/main/elements/Types.java
+++ src/main/java/com/novystxr/classysk/main/elements/Types.java
@@ -20,98 +20,96 @@ import java.util.Map.Entry;
 
 public class Types {
     public static void register() {
-        Classes.registerClass(
-                new ClassInfo<>(AbstractSkriptClass.class, "abstractclass")
-                        .user("^abstract class(es)?$")
-                        .name("Abstract Class")
-                        .description("Non-instance version of a class, holds static methods and fields, representing the class as a whole.")
-                        .parser(new Parser<>() {
+        Classes.registerClass(new ClassInfo<>(AbstractSkriptClass.class, "abstractclass")
+            .user("^abstract class(es)?$")
+            .name("Abstract Class")
+            .description("Non-instance version of a class, holds static methods and fields, representing the class as a whole.")
+            .parser(new Parser<>() {
 
-                            @Override
-                            public boolean canParse(ParseContext context) {
-                                return false;
-                            }
+                @Override
+                public boolean canParse(ParseContext context) {
+                    return false;
+                }
 
-                            @Override
-                            public String toString(AbstractSkriptClass o, int flags) {
-                                return "Abstract Class " + o.name;
-                            }
+                @Override
+                public String toString(AbstractSkriptClass o, int flags) {
+                    return "Abstract Class " + o.name;
+                }
 
-                            @Override
-                            public String toVariableNameString(AbstractSkriptClass o) {
-                                return "Abstract Class " + o.name;
-                            }
-                        })
+                @Override
+                public String toVariableNameString(AbstractSkriptClass o) {
+                    return "Abstract Class " + o.name;
+                }
+            })
         );
 
-        Classes.registerClass(
-                new ClassInfo<>(SkriptClass.class, "class")
-                        .user("^class( instance)?(es)?$")
-                        .name("Class")
-                        .description("Instance version of a class, holds non-static methods and fields, representing a created instance of a class.")
-                        .parser(new Parser<>() {
+        Classes.registerClass(new ClassInfo<>(SkriptClass.class, "class")
+            .user("^class( instance)?(es)?$")
+            .name("Class")
+            .description("Instance version of a class, holds non-static methods and fields, representing a created instance of a class.")
+            .parser(new Parser<>() {
 
-                            @Override
-                            public boolean canParse(ParseContext context) {
-                                return false;
-                            }
+                @Override
+                public boolean canParse(ParseContext context) {
+                    return false;
+                }
 
-                            @Override
-                            public String toString(SkriptClass o, int flags) {
-                                return "Class " + o.name;
-                            }
+                @Override
+                public String toString(SkriptClass o, int flags) {
+                    return "Class " + o.name;
+                }
 
-                            @Override
-                            public String toVariableNameString(SkriptClass o) {
-                                return "Class " + o.name + " (" + o.getHashCode() + ")";
-                            }
-                        })
-                        .serializer(new Serializer<>() {
-                            @Override
-                            public Fields serialize(SkriptClass o) {
-                                Fields fields = new Fields();
-                                fields.putObject("name", o.name);
+                @Override
+                public String toVariableNameString(SkriptClass o) {
+                    return "Class " + o.name + " (" + o.getHashCode() + ")";
+                }
+            })
+            .serializer(new Serializer<>() {
+                @Override
+                public Fields serialize(SkriptClass o) {
+                    Fields fields = new Fields();
+                    fields.putObject("name", o.name);
 
-                                for (Entry<String, Object[]> entry : o.getFieldValueMap().entrySet()) {
-                                    fields.putObject("field:"+entry.getKey(), entry.getValue());
-                                }
+                    for (Entry<String, Object[]> entry : o.getFieldValueMap().entrySet()) {
+                        fields.putObject("field:"+entry.getKey(), entry.getValue());
+                    }
 
-                                return fields;
-                            }
+                    return fields;
+                }
 
-                            @Override
-                            protected SkriptClass deserialize(Fields fields) throws StreamCorruptedException {
-                                String name = fields.getAndRemoveObject("name", String.class);
-                                AbstractSkriptClass parentClass = ClassManager.getClass(name);
+                @Override
+                protected SkriptClass deserialize(Fields fields) throws StreamCorruptedException {
+                    String name = fields.getAndRemoveObject("name", String.class);
+                    AbstractSkriptClass parentClass = ClassManager.getClass(name);
 
-                                SkriptClass instance;
-                                if (parentClass != null) {
-                                    instance = parentClass.createInstance();
-                                } else {
-                                    instance = new SkriptClass(name);
-                                    ClassManager.setAwaitingParent(instance);
-                                }
-                                for (FieldContext context : fields) {
-                                    if (!context.getID().startsWith("field:")) continue;
+                    SkriptClass instance;
+                    if (parentClass != null) {
+                        instance = parentClass.createInstance();
+                    } else {
+                        instance = new SkriptClass(name);
+                        ClassManager.setAwaitingParent(instance);
+                    }
+                    for (FieldContext context : fields) {
+                        if (!context.getID().startsWith("field:")) continue;
 
-                                    String fieldName = context.getID().substring("field:".length());
-                                    Object[] value = context.getObject(Object[].class);
+                        String fieldName = context.getID().substring("field:".length());
+                        Object[] value = context.getObject(Object[].class);
 
-                                    instance.putAwaitingField(fieldName, value);
-                                }
-                                return instance;
-                            }
+                        instance.putAwaitingField(fieldName, value);
+                    }
+                    return instance;
+                }
 
-                            @Override
-                            public boolean mustSyncDeserialization() {
-                                return true;
-                            }
+                @Override
+                public boolean mustSyncDeserialization() {
+                    return true;
+                }
 
-                            @Override
-                            protected boolean canBeInstantiated() {
-                                return false;
-                            }
-                        })
+                @Override
+                protected boolean canBeInstantiated() {
+                    return false;
+                }
+            })
         );
 
     }
