bb59ab66d94c2e5accae70669f8d8e6baa93c24e
nova ⭐ <novaa0@outlook.com>
2026-06-11 13:42:59 +0100

    compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    implementation("com.github.SkriptLang:Skript:2.15.3")
+    compileOnly("com.github.SkriptLang:Skript:2.15.3")
 }
 
 java {
