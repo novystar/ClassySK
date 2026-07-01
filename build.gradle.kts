2cd041996c6686cbc50746ffe38fd0dac6aeaed6
nova <novaa0@outlook.com>
2026-07-02 00:07:08 +0100

    backwards compat for java 21 and 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-    compileOnly("com.github.SkriptLang:Skript:2.15.3")
+    compileOnly("com.github.SkriptLang:Skript:2.15.0")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 tasks {
@@ -23,7 +23,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("26.1.2")
+        minecraftVersion("1.21.11")
         jvmArgs("-Xms2G", "-Xmx2G")
     }
 
