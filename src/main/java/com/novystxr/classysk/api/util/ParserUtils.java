a8c0d282296431f431895b50a896676894b15846
nova ⭐ <novaa0@outlook.com>
2026-08-13 12:02:16 +0100

    ParserUtils javadocs

--- src/main/java/com/novystxr/classysk/api/util/ParserUtils.java
+++ src/main/java/com/novystxr/classysk/api/util/ParserUtils.java
@@ -14,10 +14,24 @@ import org.skriptlang.skript.registration.SyntaxInfo;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.function.Supplier;
 
 public class ParserUtils {
 
+    /**
+     *
+     * Parses expressions and section expressions.
+     * This method properly modifies the section context for the given node, if necessary.
+     * Designed to support nested section parsing.
+     *
+     * @param rawExpr Raw string to parse
+     * @param node The node belonging to this syntax
+     * @param types Types to convert to
+     *
+     * @see SectionContext#modify(SectionNode, List, Supplier)
+     * @see SkriptParser#parseExpression(Class[])
+     */
     public static @Nullable Expression<?> parseExprNode(String rawExpr, Node node, Class<?>... types) {
         SectionContext context = ParserInstance.get().getData(SectionContext.class);
 
@@ -34,6 +48,20 @@ public class ParserUtils {
         return LiteralUtils.canInitSafely(expr) ? expr : null;
     }
 
+    /**
+     *
+     * Parses a node as any of the given syntax infos.
+     * Takes into account section context and filters the target syntaxes depending on node type.
+     * For example a section node will only parse as infos of {@link Section}.
+     * <br> <br>
+     * This only supports Statements and Sections where the node's key is parsed in whole.
+     * For expression parsing see {@link ParserUtils#parseExprNode(String, Node, Class[])}.
+     *
+     * @param node The target node, can be a SectionNode
+     * @param defaultError The default error to print if the syntax didn't match
+     * @param infos The target syntax infos
+     * @return The parsed syntax element - null if failed to parse
+     */
     public static <T extends SyntaxElement> @Nullable T parseNodeAsInfos(Node node, @Nullable String defaultError, SyntaxInfo<?>... infos) {
         SkriptLogger.setNode(node);
         String expr = node.getKey();
@@ -49,6 +77,15 @@ public class ParserUtils {
         }
     }
 
+    /**
+     *
+     * Helper method to get an iterator of syntax infos that can be used with skript's parser methods
+     *
+     * @param infos The syntax infos to iterate
+     * @param filterAs Syntax types which should be included, if empty nothing will be filtered
+     *
+     * @see SkriptParser#parse(String, Iterator, String)
+     */
     @SuppressWarnings("unchecked")
     public static <T extends SyntaxElement> Iterator<SyntaxInfo<T>> infoIterator(SyntaxInfo<?>[] infos, Class<?>... filterAs) {
         return (Iterator<SyntaxInfo<T>>) (Iterator<?>) Arrays.stream(infos)
