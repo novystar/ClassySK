decd7bb19df62972aae707cfad6e8b526d102b62
nova <novaa0@outlook.com>
2026-08-14 12:11:12 +0100

    equals override for TypedInstanceWrapper

--- src/main/java/com/novystxr/classysk/api/classes/TypedInstanceWrapper.java
+++ src/main/java/com/novystxr/classysk/api/classes/TypedInstanceWrapper.java
@@ -31,15 +31,10 @@ public class TypedInstanceWrapper implements TypeWrappable<TypedInstanceWrapper,
     }
 
     @Override
-    public int hashCode() {
-        return unwrap().hashCode();
-    }
-
-    @Override
     public boolean equals(Object obj) {
         if (obj instanceof TypeWrappable<?,?> wrappable) {
-            return wrappable.unwrap().equals(wrappable.unwrap());
+            return unwrap().equals(wrappable.unwrap());
         }
-        return false;
+        return obj.equals(this);
     }
 }
